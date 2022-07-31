94eb115714523253a47e61729d88b41e94726223
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-07-31 07:40:37 +0700

    feat: card akun bank

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import HeaderDashboard from '~/components/header-dashboard'
 import { MdOutlineMapsHomeWork } from 'react-icons/md'
-import { BsShare } from 'react-icons/bs'
+import { BsShare, BsTrash } from 'react-icons/bs'
+import { BiPencil } from 'react-icons/bi'
+import { SiVisa } from 'react-icons/si'
 import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
@@ -58,16 +60,51 @@ const Dashboard = () => {
           </ol>
         </div>
         <div className="bg-white shadow rounded p-4">
-          <header>
-            <span>Lokasi 3</span>
-            <span>Lihat Semua</span>
+          <header className="flex items-center justify-between py-3">
+            <span className="font-semibold">Akun Bank</span>
+            <button className=" bg-emerald-600 py-1 px-4 rounded transition-transform ease-in hover:scale-105 duration-150  ">
+              <span className="text-white text-sm">+ Tambah Akun Bank</span>
+            </button>
           </header>
-          <ol>
-            <li className="bg-emerald-400 rounded-md px-3 py-4 flex items-center justify-between text-white">
-              <MdOutlineMapsHomeWork className=" w-10 h-10" />
-              <div>
-                <p>20</p>
-                <p>Induk</p>
+          <ol className=" space-y-7 mt-2">
+            <li className="flex items-start justify-between h-[100px]">
+              <div className=" flex items-end justify-end p-2 w-[150px] h-full rounded-md bg-gradient-to-br from-yellow-200  to-emerald-600">
+                <SiVisa className="w-10 h-10 text-white" />
+              </div>
+              <div className="flex flex-col h-full justify-between">
+                <p className="font-bold">Bank KB Bukopin</p>
+                <div className=" text-gray-400 text-sm">
+                  <p>***** 5525 - Si Tampan</p>
+                  <p className=" uppercase">USD</p>
+                </div>
+              </div>
+              <div className='flex items-center space-x-2'>
+                <button>
+                  <BiPencil className="w-4 h-4 text-emerald-400" />
+                </button>
+                <button>
+                  <BsTrash className="w-4 h-4 text-red-400" />
+                </button>
+              </div>
+            </li>
+            <li className="flex items-start justify-between h-[100px]">
+              <div className=" flex items-end justify-end p-2 w-[150px] h-full rounded-md bg-gradient-to-br from-yellow-200  to-emerald-600">
+                <SiVisa className="w-10 h-10 text-white" />
+              </div>
+              <div className="flex flex-col h-full justify-between">
+                <p className="font-bold">Bank KB Bukopin</p>
+                <div className=" text-gray-400 text-sm">
+                  <p>***** 5525 - Si Tampan</p>
+                  <p className=" uppercase">USD</p>
+                </div>
+              </div>
+              <div className='flex items-center space-x-2'>
+                <button>
+                  <BiPencil className="w-4 h-4 text-emerald-400" />
+                </button>
+                <button>
+                  <BsTrash className="w-4 h-4 text-red-400" />
+                </button>
               </div>
             </li>
           </ol>
@@ -97,18 +134,14 @@ const Dashboard = () => {
               <p className=" text-base break-words text-gray-500 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis unde corrupti dolorem voluptatum, animi fugit!</p>
               <span className=" text-sm text-gray-300">Hari ini, 06:00</span>
             </li>
-            <li>
-              <p className=" text-base break-words text-gray-500 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis unde corrupti dolorem voluptatum, animi fugit!</p>
-              <span className=" text-sm text-gray-300">Hari ini, 06:00</span>
-            </li>
           </ol>
         </div>
-        <div className="bg-white shadow rounded p-4 max-h-64">
+        <div className="bg-white shadow rounded p-4">
           <header className="flex items-center justify-between py-3">
             <span className="font-semibold">Relasi</span>
             <Link to="/dashboard/home" className=" text-emerald-600 text-base">Lihat semua</Link>
           </header>
-          <ol className="mt-2 space-y-4">
+          <ol className="mt-3 space-y-6">
             <li className="flex items-center space-x-3">
               <BsShare className="w-5 h-5" />
               <div>
